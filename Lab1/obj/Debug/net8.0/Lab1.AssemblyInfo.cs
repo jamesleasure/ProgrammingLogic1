fa5980bfe56acaf3d1b5b15fa7888770db206626
James Leasure <james.leasure@tri-c.edu>
2025-03-18 19:25:30 +0000

    Modified Lab 1

--- Lab1/obj/Debug/net8.0/Lab1.AssemblyInfo.cs
+++ Lab1/obj/Debug/net8.0/Lab1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Lab1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2accd871e06fa8a2620cb9d2f8bc4744c84a77fe")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+03be659693e678e2870d787439fedcb0db4d34c0")]
 [assembly: System.Reflection.AssemblyProductAttribute("Lab1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Lab1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
